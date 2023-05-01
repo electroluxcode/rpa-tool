46d9cc5806ff193103b569e6ee15f6c0512b97ca
Electrolux <i_zengpei@cvte.com>
2023-05-01 17:58:32 +0800

    test:添加教学测试案例

--- phoneRPA/phoneRPA.py
+++ phoneRPA/phoneRPA.py
@@ -126,7 +126,7 @@ if __name__ == '__main__':
     print('Electrolux_Phone_RPA欢迎使用')
     # 数据检查
     # checkCmd = dataCheck(allData)
-    f = open('自动发送.json', encoding='UTF-8')
+    f = open('phoneData.json', encoding='UTF-8')
     allData = json.load(f)
     key = input('选择功能: 1.做一次 2.循环到死 \n')
     if key == '1':
