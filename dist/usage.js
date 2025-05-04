da0abaf15c3bf9e6cbeb8cc9f9edfe1557174537
electroluxcode <3451613934@qq.com>
2025-05-05 02:50:13 +0800

    backup

--- dist/usage.js
+++ dist/usage.js
@@ -1,27 +1,18 @@
 const { exec } = require('child_process');
+const { describe, it, expect } = require("vitest")
 
-const data =  [
-  {
-    "cmdType": "Click",
-    "cmdParam": {
-        "x": 100,
-        "y": 100,
-        "clicks": 2
-    }
-},
-]
 
-  const normalJsonStr = JSON.stringify(data)
-  const escapedJsonStr = (normalJsonStr).replaceAll("\"", "\\\"").replaceAll("\\\\", "\\")
-  console.log("'" +escapedJsonStr+ "'");
-  
-  exec('pcRPAToolCli.exe ' + escapedJsonStr, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`执行的错误: ${error}`);
-      return;
-    }
-    console.log(`标准输出: ${stdout}`);
-    if (stderr) {
-      console.error(`标准错误输出: ${stderr}`);
-    }
-  });
+const testCase1 = ()=>{
+  return new Promise((resolve,reject)=>{
+    exec('pcRPAToolCli.exe ' + data, (error, stdout, stderr) => {
+      resolve(stdout)
+    });
+  })
+}
+
+describe("用例1", () => {
+  it("测试是否能够清空云文档", async () => {
+    const result = await testCase1()
+    expect(result).toEqual("no error")
+  })
+})
